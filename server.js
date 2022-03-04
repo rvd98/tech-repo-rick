81aad8c79fc8a08cb53a3e614e7be2b45cd1f7f0
rvd98 <rick.van.dijken@hotmail.com>
2022-03-04 01:30:08 +0100

    aan de HTML en CSS begonnen

--- server.js
+++ server.js
@@ -2,22 +2,24 @@ const express = require('express')
 const { engine } = require('express-handlebars');
 const app = express()
 
+const path = require('path');
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
 app.use(express.static('public'))
 
 app.engine('.hbs', engine({
-    extname: '.hbs',
-    defaultLayoyt: 'main'
-  }));
-  app.set('view engine', '.hbs');
-  app.set("views", "./views");
-  
-  app.get('/', (req, res) => {
-      res.render('home');
-  });
+  extname: '.hbs',
+  defaultLayoyt: 'main'
+}));
+app.set('view engine', '.hbs');
+app.set("views", "./views");
 
-  app.get('/liked', (req, res) => {
-    res.render('liked');
+app.get('/', (req, res) => {
+  res.render('home');
 });
-  
-  app.listen(3000);
-  
+
+app.get('/liked', (req, res) => {
+  res.render('liked');
+});
+
+app.listen(3000);
