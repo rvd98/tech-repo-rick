8966f8e2857e75ac11edef6530db9aeb01f09992
rvd98 <rick.van.dijken@hotmail.com>
2022-03-14 23:46:28 +0100

    edited server.js and home.hbs

--- server.js
+++ server.js
@@ -1,11 +1,35 @@
 const express = require('express');
 const { engine } = require('express-handlebars');
-const bodyParser = require('body-parser');
 const app = express()
 const port = process.env.PORT || 1337;
 require('dotenv').config();
 const connectDB = require('./config/db')
 connectDB();
+
+const productData = {
+  boards:{
+    TH_bluewaves: {
+      name: 'Blue waves',
+      creator: 'Tony Hawk',
+      price: '95',
+    },
+    RS_redire: {
+      name: 'RedFire',
+      creator: 'Ryan Sheckler', 
+      price: '80'
+    },
+    NH_brokenglass: {
+      name: 'BrokenGlass', 
+      creator: 'Nyjah Houston', 
+      price: '105'
+    }
+  }
+}
+
+const userData = {
+  likes: []
+}
+
 
 const path = require('path');
 app.use('/static', express.static(path.join(__dirname, 'public')));
@@ -20,7 +44,7 @@ app.set('view engine', '.hbs');
 app.set("views", "./views");
 
 app.get('/', (req, res) => {
-  res.render('home');
+  res.render('home', { boardData: productData.boards });
 });
 
 app.get('/liked', (req, res) => {
